aba5715765d9216f4d7658a1411352f5ffcee0bb
Vito and Jeffery <jeffery.lu@oocl.com>
2019-01-07 23:10:17 +0800

    [Jeffery] install recharts and add some simple demo

--- hello-react-app/src/App.js
+++ hello-react-app/src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {SimpleLineChart} from "./rechartsDemo/SimpleLineChart";
+import {SimpleBarChart} from "./rechartsDemo/SimpleBarChart";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          Welcome to React.
+          <a className="App-link" href="https://reactjs.org"
+            target="_blank" rel="noopener noreferrer">
             Learn React
-          </a>
+          </a><br/>
         </header>
+        <div style={{padding: 20}}>
+          <h2>Recharts Demo</h2>
+          <p>1. SimpleLineChart</p>
+          <SimpleLineChart/>
+          <p>2. SimpleBarChart</p>
+          <SimpleBarChart/>
+        </div>
       </div>
     );
   }
